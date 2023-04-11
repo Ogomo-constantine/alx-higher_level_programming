0a7298fd418c3cbed4bde6e4c68e0fabebd31c57
Ogomo-constantine <ogomoshiro@gmail.com>
2023-04-11 09:03:28 -0700

    inheritance object

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+Class object:
+    def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or a subclass of a_class,
+    otherwise return False.
+    """
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+Class object:
+    def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited (directly or indirectly) from a_class.
+    Otherwise, returns False.
+    """
+    # Check if obj is an instance of a_class
+    if isinstance(obj, a_class):
+        return True
+    # Check if obj's class inherits from a_class
+    if issubclass(type(obj), a_class):
+        return True
+    # If neither case is true, return False
+    return False
